3b80baa1794af2847fd72459cb3bdad1bdd3f81c
KevinL135 <luokevin135@gmail.com>
2026-03-21 17:42:44 -0400

    Added changing email function

--- python_app.py
+++ python_app.py
@@ -13,6 +13,12 @@ supabase: Client = create_client(
     os.environ.get("SUPABASE_PUBLISHABLE_KEY")
 )
 
+@app.post("/update-email")
+def update_email(id, new_email):
+    response = supabase.table("Test_Database_1").update({"email": new_email}).eq("id", id).execute()
+    print(response) 
+    return response
+
 @app.route('/')
 def index():
     response = supabase.table('Test_Database_1').select("*").execute()
@@ -20,7 +26,7 @@ def index():
 
     html = '<h1>Users</h1><ul>'
     for user in users:
-        html += f'<li>Name: {user["name"]}, Topics:blank</li>'
+        html += f'<li>Name: {user["name"]}, Email: {user["email"]}, Topics: {user["topics"]["topics"][0]["topic_name"]}</li>'
     html += '</ul>'
 
     return html
